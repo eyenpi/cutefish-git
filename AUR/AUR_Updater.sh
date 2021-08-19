7e893f22a74f8c9cd5f43aa8566dca08082bcf62
Eyenpi <eyenpix@gmail.com>
2021-08-19 16:10:00 +0430

    update the updater

--- AUR/AUR_Updater.sh
+++ AUR/AUR_Updater.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
-for f in cutefish*; do
+for f in *-git/; do
 	if [ -d "$f" ]; then
+        cd $f
+        makepkg --noarchive -oCcf --noconfirm
+        find . ! -name 'PKGBUILD' -type f,d -exec rm -rf {} +
         cd ~/w/cutefish-git-aur/$f
 		cp ~/w/cutefish-git/$f/PKGBUILD .
         cp ~/w/cutefish-git/AUR/aur.gitignore .
         mv aur.gitignore .gitignore
         makepkg --printsrcinfo > .SRCINFO
+        git add PKGBUILD .SRCINFO .gitignore
+        git commit -m "updated to version 0.4"
+        git push
         cd ~/w/cutefish-git
 	fi
 done
