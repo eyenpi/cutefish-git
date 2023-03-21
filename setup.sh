eab632a66c6f71a328e726cd3bbc4b8d380085e5
Ali Nabipour <alinabipour01@gmail.com>
2023-03-21 17:31:54 +0330

    update the packages to install

--- setup.sh
+++ setup.sh
@@ -1,13 +1,16 @@
-cd fishui-git
-makepkg -si --noconfirm
-cd ../libcutefish-git
-makepkg -si --noconfirm
-cd ..
+#!/bin/bash
 
-for f in cutefish*; do
-	if [ -d "$f" ]; then
-		cd $f
-		makepkg -si --noconfirm
-		cd ..
-	fi
+packages=(
+            "fishui-git" "libcutefish-git" "cutefish-core-git" "cutefish-calculator-git"
+            "cutefish-dock-git" "cutefish-filemanager-git" "cutefish-icons-git"
+            "cutefish-kwin-plugins-git" "cutefish-launcher-git" "cutefish-qt-plugins-git"
+            "cutefish-screenlocker-git" "cutefish-settings-git" "cutefish-statusbar-git"
+            "cutefish-terminal-git" "cutefish-wallpapers-git" "cutefish-screenshot-git"
+            "cutefish-videoplayer-git" "cutefish-gtk-themes-git" "cutefish-cursor-themes-git"
+        )
+
+for f in ${packages[@]}; do
+    cd $f
+	makepkg -si --noconfirm
+	cd ..
 done
